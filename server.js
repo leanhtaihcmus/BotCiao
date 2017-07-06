b074e8bce874121d8d38e4a0e8a763f7bb21a35c
Lê Anh Tài <leanhtaihcmus@gmail.com>
2017-07-06 17:19:28 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const https = require('https');
 const request = require('request');
 
 var app = express();
-app.set('port', process.env.PORT);
+app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({verify: verifyRequestSignature }));
 app.use(express.static('public'));
@@ -82,6 +82,36 @@ app.post('/webhook', function(req, res) {
   }
 });
 
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from 
+ * the App Dashboard, we can verify the signature that is sent with each 
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an 
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
 
 function receivedMessage(event) {
 
@@ -150,3 +180,12 @@ function callSendAPI(messageData) {
     }
   });
 }
+
+// Start server
+// Webhooks must be available via SSL with a certificate signed by a valid 
+// certificate authority.
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+module.exports = app;
